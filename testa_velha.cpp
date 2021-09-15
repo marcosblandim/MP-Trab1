f4f395555a3bb5d8f30b37989c500ac4647b0861
marcos.blandim <marcos.blandim@seatecnologia.com.br>
2021-09-15 12:23:40 -0300

    novo teste "X vence"; apagando teste "Testa velha"

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -7,14 +7,24 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-  
 
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
-	int teste1[3][3]= {   { 2, 0, 1 }, 
-	                      { 2, 0, 1 },
-						  { 0, 2, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste1) == 1 );
+TEST_CASE( "X vence", "[single-file]" ) {
+    int xVence1[3][3]= {   { 1, 0, 0 },
+                           { 2, 1, 0 },
+                           { 2, 2, 1 }
+    };
+    int xVence2[3][3]= {   { 1, 1, 2 },
+                           { 1, 0, 2 },
+                           { 1, 2, 0 }
+    };
+    int xVence3[3][3]= {   { 2, 0, 0 },
+                           { 2, 1, 2 },
+                           { 1, 1, 1 }
+    };
+
+    REQUIRE( VerificaVelha(xVence1) == 1 );
+    REQUIRE( VerificaVelha(xVence2) == 1 );
+    REQUIRE( VerificaVelha(xVence3) == 1 );
 }
 
